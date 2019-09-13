7671286095c4438ac88b7f40f6ed17501a59f6d9
ippeyo <ippeyo.tech@gmal.com>
2019-09-14 00:24:05 +0900

    while-edit
    while requesting the added action

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
   function buildMessageHtml(message){
-    var addImage = (message.image !== null) ? `<img class = "lower-message__image", src="${message.image.url}">` : '' ;
+    var addImage = (message.image.url !== null) ? `<img class = "lower-message__image", src="${message.image.url}">` : '' ;
     var html = `<div class="message" data-message-id="${message.id}">
                   <div class="message__upper-info">
                     <div class="message__upper-info__talker">
@@ -21,6 +21,32 @@ $(function(){
                 <div>`
     return html
   }
+
+  var interval = setInterval(function() {
+    if (location.href.match(/\/groups\/\d+\/messages/)){
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      var last_message_id = $('.message').last().data('id');
+      var href = 'api/messages'
+      $.ajax({
+        url: href,
+        type: "GET",
+        data: {id: last_message_id},
+        dataType: "json"
+      })
+      .done(function(messages) {
+        messages.forEach(function(message) {
+          var insertHTML = buildHTML(message)
+          $('#message').append(insertHTML)
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        })
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+      });
+    } else {
+        clearInterval(interval);
+      }
+  } , 5000 );
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -46,5 +72,5 @@ $(function(){
       alert('error');
       $('.submit-btn').prop('disabled', false);
     })
-  })
-})
+  });
+});
